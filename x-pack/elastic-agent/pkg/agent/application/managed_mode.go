c510d8b085a8ef5e1c480ec76a07c783c9064cce
Michal Pristas <michal.pristas@gmail.com>
2021-06-07 12:11:59 +0200

    drop

--- x-pack/elastic-agent/pkg/agent/application/managed_mode.go
+++ x-pack/elastic-agent/pkg/agent/application/managed_mode.go
@@ -7,6 +7,8 @@ package application
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/elastic/go-sysinfo"
 
@@ -80,11 +82,13 @@ func newManaged(
 	statusCtrl status.Controller,
 	agentInfo *info.AgentInfo,
 ) (*Managed, error) {
+	checkBinary(log, "nm.1")
 	caps, err := capabilities.Load(paths.AgentCapabilitiesPath(), log, statusCtrl)
 	if err != nil {
 		return nil, err
 	}
 
+	checkBinary(log, "nm.2")
 	client, err := client.NewAuthWithConfig(log, cfg.Fleet.AccessAPIKey, cfg.Fleet.Client)
 	if err != nil {
 		return nil, errors.New(err,
@@ -93,6 +97,7 @@ func newManaged(
 			errors.M(errors.MetaKeyURI, cfg.Fleet.Client.Host))
 	}
 
+	checkBinary(log, "nm.3")
 	sysInfo, err := sysinfo.Host()
 	if err != nil {
 		return nil, errors.New(err,
@@ -105,11 +110,14 @@ func newManaged(
 		agentInfo: agentInfo,
 	}
 
+	checkBinary(log, "nm.4")
 	managedApplication.bgContext, managedApplication.cancelCtxFn = context.WithCancel(ctx)
 	managedApplication.srv, err = server.NewFromConfig(log, cfg.Settings.GRPC, &operation.ApplicationStatusHandler{})
 	if err != nil {
 		return nil, errors.New(err, "initialize GRPC listener", errors.TypeNetwork)
 	}
+
+	checkBinary(log, "nm.4")
 	// must start before `Start` is called as Fleet will already try to start applications
 	// before `Start` is even called.
 	err = managedApplication.srv.Start()
@@ -117,29 +125,38 @@ func newManaged(
 		return nil, errors.New(err, "starting GRPC listener", errors.TypeNetwork)
 	}
 
+	checkBinary(log, "nm.5")
 	logR := logreporter.NewReporter(log)
+
+	checkBinary(log, "nm.6")
 	fleetR, err := fleetreporter.NewReporter(agentInfo, log, cfg.Fleet.Reporting)
 	if err != nil {
 		return nil, errors.New(err, "fail to create reporters")
 	}
 
+	checkBinary(log, "nm.7")
 	combinedReporter := reporting.NewReporter(managedApplication.bgContext, log, agentInfo, logR, fleetR)
+
+	checkBinary(log, "nm.8")
 	monitor, err := monitoring.NewMonitor(cfg.Settings)
 	if err != nil {
 		return nil, errors.New(err, "failed to initialize monitoring")
 	}
 
+	checkBinary(log, "nm.9")
 	router, err := router.New(log, stream.Factory(managedApplication.bgContext, agentInfo, cfg.Settings, managedApplication.srv, combinedReporter, monitor, statusCtrl))
 	if err != nil {
 		return nil, errors.New(err, "fail to initialize pipeline router")
 	}
 	managedApplication.router = router
 
+	checkBinary(log, "nm.10")
 	composableCtrl, err := composable.New(log, rawConfig)
 	if err != nil {
 		return nil, errors.New(err, "failed to initialize composable controller")
 	}
 
+	checkBinary(log, "nm.11")
 	emit, err := emitter.New(
 		managedApplication.bgContext,
 		log,
@@ -156,26 +173,33 @@ func newManaged(
 	if err != nil {
 		return nil, err
 	}
+	checkBinary(log, "nm.12")
 	acker, err := fleet.NewAcker(log, agentInfo, client)
 	if err != nil {
 		return nil, err
 	}
 
+	checkBinary(log, "nm.13")
 	batchedAcker := lazy.NewAcker(acker, log)
 
+	checkBinary(log, "nm.14")
 	// Create the state store that will persist the last good policy change on disk.
 	stateStore, err := store.NewStateStoreWithMigration(log, paths.AgentActionStoreFile(), paths.AgentStateStoreFile())
 	if err != nil {
 		return nil, errors.New(err, fmt.Sprintf("fail to read action store '%s'", paths.AgentActionStoreFile()))
 	}
+
+	checkBinary(log, "nm.15")
 	managedApplication.stateStore = stateStore
 	actionAcker := store.NewStateStoreActionAcker(batchedAcker, stateStore)
 
+	checkBinary(log, "nm.16")
 	actionDispatcher, err := dispatcher.New(managedApplication.bgContext, log, handlers.NewDefault(log))
 	if err != nil {
 		return nil, err
 	}
 
+	checkBinary(log, "nm.17")
 	managedApplication.upgrader = upgrade.NewUpgrader(
 		agentInfo,
 		cfg.Settings.DownloadConfig,
@@ -241,6 +265,7 @@ func newManaged(
 
 	actions := stateStore.Actions()
 	stateRestored := false
+	checkBinary(log, "nm.18")
 	if len(actions) > 0 && !managedApplication.wasUnenrolled() {
 		// TODO(ph) We will need an improvement on fleet, if there is an error while dispatching a
 		// persisted action on disk we should be able to ask Fleet to get the latest configuration.
@@ -251,6 +276,7 @@ func newManaged(
 		stateRestored = true
 	}
 
+	checkBinary(log, "nm.19")
 	gateway, err := fleetgateway.New(
 		managedApplication.bgContext,
 		log,
@@ -265,15 +291,19 @@ func newManaged(
 	if err != nil {
 		return nil, err
 	}
+	checkBinary(log, "nm.20")
 	gateway, err = localgateway.New(managedApplication.bgContext, log, cfg.Fleet, rawConfig, gateway, emit, !stateRestored)
 	if err != nil {
 		return nil, err
 	}
+	checkBinary(log, "nm.21")
 	// add the acker and gateway to setters, so the they can be updated
 	// when the hosts for Fleet Server are updated by the policy.
 	if cfg.Fleet.Server == nil {
 		// setters only set when not running a local Fleet Server
+		checkBinary(log, "nm.22")
 		policyChanger.AddSetter(gateway)
+		checkBinary(log, "nm.23")
 		policyChanger.AddSetter(acker)
 	}
 
@@ -288,26 +318,30 @@ func (m *Managed) Routes() *sorted.Set {
 
 // Start starts a managed elastic-agent.
 func (m *Managed) Start() error {
+	checkBinary(m.log, "s.1")
 	m.log.Info("Agent is starting")
 	if m.wasUnenrolled() {
 		m.log.Warnf("agent was previously unenrolled. To reactivate please reconfigure or enroll again.")
 		return nil
 	}
-
+	checkBinary(m.log, "s.2")
 	// reload ID because of win7 sync issue
 	if err := m.agentInfo.ReloadID(); err != nil {
 		return err
 	}
 
+	checkBinary(m.log, "s.3")
 	err := m.upgrader.Ack(m.bgContext)
 	if err != nil {
 		m.log.Warnf("failed to ack update %v", err)
 	}
+	checkBinary(m.log, "s.4")
 
 	err = m.gateway.Start()
 	if err != nil {
 		return err
 	}
+	checkBinary(m.log, "s.5")
 	return nil
 }
 
@@ -334,4 +368,17 @@ func (m *Managed) wasUnenrolled() bool {
 	}
 
 	return false
+}
+
+func checkBinary(log *logger.Logger, point string) {
+	pid := os.Getpid()
+	fn := filepath.Join(paths.Top(), paths.BinaryName)
+	_, err := os.Stat(fn)
+	suffix := "ok"
+
+	if os.IsNotExist(err) {
+		suffix = "not found"
+	}
+
+	log.Errorf(">>> [%d].%s %s %s", point, pid, fn, suffix)
 }
