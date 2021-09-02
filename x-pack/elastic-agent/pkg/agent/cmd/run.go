e6c6735b2dd4faa62b9fd97a13473f51137e4664
Michal Pristas <michal.pristas@gmail.com>
2021-09-02 16:30:18 +0200

    await execution on exit

--- x-pack/elastic-agent/pkg/agent/cmd/run.go
+++ x-pack/elastic-agent/pkg/agent/cmd/run.go
@@ -59,19 +59,20 @@ func newRunCommandWithArgs(_ []string, streams *cli.IOStreams) *cobra.Command {
 	}
 }
 
-func run(streams *cli.IOStreams, override cfgOverrider) error { // Windows: Mark service as stopped.
-	locker := filelock.NewAppLocker(paths.Data(), paths.AgentLockFileName)
-	if err := locker.TryLock(); err != nil {
-		return err
-	}
-	defer locker.Unlock()
-
+func run(streams *cli.IOStreams, override cfgOverrider) error {
+	// Windows: Mark service as stopped.
 	// After this is run, the service is considered by the OS to be stopped.
 	// This must be the first deferred cleanup task (last to execute).
 	defer func() {
 		service.NotifyTermination()
 		service.WaitExecutionDone()
 	}()
+
+	locker := filelock.NewAppLocker(paths.Data(), paths.AgentLockFileName)
+	if err := locker.TryLock(); err != nil {
+		return err
+	}
+	defer locker.Unlock()
 
 	service.BeforeRun()
 	defer service.Cleanup()
